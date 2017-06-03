2e31b2e48c8cbc07b0bdc8e220a2e55974a0ac04
Noah Kaplan <noahkaplan4@gmail.com>
2017-06-03 02:53:03 -0400

    get facebook friend names

--- contacts.py
+++ contacts.py
@@ -11,3 +11,12 @@ with open ('contacts.vcf', 'r') as contacts_file:
 
 
 # get contacts from Facebook
+
+with open ('facebook_friends.txt', 'r') as facebook_friends_file:
+
+    for line in facebook_friends_file:
+
+        if 'FriendFriends' not in line:
+            continue
+
+        print facebook_friends_file.next().strip()
